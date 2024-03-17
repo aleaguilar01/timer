c55bb0b4763c7ae732a8fd3353e7a652820a763f
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-03-17 00:12:43 -0600

    Adding timer into a function

--- timer1.js
+++ timer1.js
@@ -1,12 +1,16 @@
 const args = process.argv.slice(2);
 
-if (args.length === 0) {
-  return;
-}
-
-for (let time of args) {
-  if (time > 0 && !Number.isNaN(time)) {
-    time *= 1000;
-    setTimeout(() => process.stdout.write('\x07'), time);
+const timer = (times) => {
+  if (times.length === 0) {
+    return;
   }
-}
+  
+  for (let time of times) {
+    if (time > 0 && !Number.isNaN(time)) {
+      time *= 1000;
+      setTimeout(() => process.stdout.write('\x07'), time);
+    }
+  }
+};
+
+timer(args);
